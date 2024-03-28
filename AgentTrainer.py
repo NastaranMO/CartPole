05d4689067e159f772c52440907a8e5da86f7801
Nastaran Moghadasi <nastaran@Nastarans-MacBook-Pro.local>
2024-03-28 20:21:12 +0100

    add pydoc to agenttrainer

--- AgentTrainer.py
+++ AgentTrainer.py
@@ -80,6 +80,17 @@ def main(raw_args=None):
 
 
 def DQN_learning(env, args):
+    """
+    This function perofrms the training of a DQN agent on the environment,
+    optionally using a experience replay (ER) and/or a target network (TN) to enhance the learning process.
+
+    Parameters:
+    - env: the environment to train the agent in.
+    - args: A namespace containing parameters such as 'learning rate' or 'steps'.
+
+    Returns:
+    - reward_means: A list of average rewards obtained during evaluation intervals.
+    """
     agent = DQN_Agent(**vars(args))
     reward_means = []
     for e in range(args.num_episodes):
@@ -132,10 +143,34 @@ def DQN_learning(env, args):
 
 
 def is_pure_DQN(ER, TN):
+    """
+    Check if the algorithm is pure DQN without experience replay or target network.
+
+    Parameters:
+    - ER (bool): experience replay.
+    - TN (bool): target network.
+
+    Returns:
+    - bool: True if the algorithm is pure DQN, False otherwise.
+    """
     return ER == False and TN == False
 
 
 def dqn_er(args, agent):
+    """
+    DQN with experience replay (DQN-ER) algorithm.
+    In case of both args.TN and args.ER are True, DQN with experience replay (DQN-ER) and target network (TN) algorithm.
+
+    This function updates the agent's policy network by sampling a batch of experiences from the replay buffer,
+    it calculates the loss between the predicted Q-values and the target Q-values, and performs gradient descent step.
+    In case of args.TN, the target network is updated less frequently than the policy network.
+
+    Parameters:
+    - args: A namespace containing parameters such as 'learning rate' or 'steps'.
+    - agent: the DQN agent.
+        - ER (bool): experience replay.
+        - TN (bool): target network.
+    """
     # Sample a batch of experiences
     experiences = agent.get_sample()
     states_tuple, actions_tuple, next_states_tuple, rewards_tuple = zip(
@@ -175,6 +210,16 @@ def dqn_er(args, agent):
 
 
 def pure_DQN(agent, state, action, next_state, reward):
+    """
+    Pure DQN algorithm without experience replay or target network.
+
+    Parameters:
+    - agent: the DQN agent.
+    - state (Tensor): the current state.
+    - action (Tensor): the action taken.
+    - next_state (Tensor or None): the next state.
+    - reward (Tensor): the reward received.
+    """
     predicted_q_values = agent.policy_net(state).gather(1, action)
     next_state_values = torch.zeros(1)
     if next_state is not None:
@@ -191,6 +236,21 @@ def pure_DQN(agent, state, action, next_state, reward):
 
 
 def dqn_TN(agent, state, action, next_state, reward, args):
+    """
+    DQN Target Network (DQN-TN) algorithm.
+
+    This function computes the loss between predicted Q-values from the policy-network and the target Q-values,
+    which are derived using the target network. The target Q-values are updated less frequently than
+    the policy network.
+
+    Parameters:
+    - agent: the DQN agent.
+    - state (Tensor): the current state.
+    - action (Tensor): the action taken.
+    - next_state (Tensor or None): the next state.
+    - reward (Tensor): the reward received.
+    - args: A namespace containing parameters such as 'learning rate' or 'steps'.
+    """
     predicted_q_values = agent.policy_net(state).gather(1, action)
     next_state_values = torch.zeros(1)
     if next_state is not None:
@@ -209,6 +269,16 @@ def dqn_TN(agent, state, action, next_state, reward, args):
 
 
 def average_over_repetitions(env, args):
+    """
+    Run the experiment for a number of repetitions and average the results
+
+    Parameters:
+    - env: the environment to run the experiment in
+    - args: the command line arguments includes learning rate, number of episodes, etc.
+
+    Returns:
+    - np.array: the average return over the repetitions along with the standard deviation
+    """
     smoothing_window = 9
 
     print(f"Running with arguments =====>: {args}")
